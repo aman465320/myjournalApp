7d9bc18b675be52d514293c529e9f3fffc03d1e9
Aman Mathur <amanmathur.2612@gmail.com>
2024-12-10 23:53:20 +0530

    Revert "initial commit"
    This reverts commit 9eac3204fdb346abaef4eac26e55021f44f3556c.

--- src/main/java/com/example/myJournalApp/repository/UserRespository.java
+++
@@ -1,9 +0,0 @@
-package com.example.myJournalApp.repository;
-
-import com.example.myJournalApp.entity.UserEntity;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRespository extends MongoRepository<UserEntity, ObjectId> {
-  UserEntity findByUserName(String userName);
-}
